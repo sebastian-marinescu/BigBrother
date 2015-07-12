c4f0499e6162625aa5db70966307ffa092662e6d
Mark Hamstra <hello@markhamstra.com>
2015-07-13 01:53:21 +0200

    1.3.0-pl

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -15,7 +15,7 @@ set_time_limit(0);
 define('PKG_NAME','BigBrother');
 define('PKG_NAMESPACE','bigbrother');
 define('PKG_VERSION','1.3.0');
-define('PKG_RELEASE','rc3');
+define('PKG_RELEASE','pl');
 
 function getSnippetContent($path, $name, $debug = false) {
     $name = ($debug) ? 'debug.'. $name .'.php' : $name .'.php';
