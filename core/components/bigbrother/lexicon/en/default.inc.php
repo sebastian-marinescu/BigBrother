aa1b2c91472d23d902cef212d2023ff81855c261
digitalpenguin <murray@digitalpenguin.hk>
2021-06-05 16:17:03 +0800

    Add lexicons for settings.

--- core/components/bigbrother/lexicon/en/default.inc.php
+++ core/components/bigbrother/lexicon/en/default.inc.php
@@ -6,45 +6,26 @@
  * @subpackage lexicon
  *
  */
-$_lang['bigbrother'] = 'Google Analytics';
-
-/* Menu */
-$_lang['bigbrother.admin'] = 'Google Analytics';
-$_lang['bigbrother.menu_desc'] = 'View your site reports directly from the manager';
+$_lang['bigbrother'] = 'Google Analytics v4 by Big Brother';
 
 /* System settings */
-$_lang['setting_bigbrother.account'] = 'Analytics Profile';
-$_lang['setting_bigbrother.account_desc'] = 'ID of the profile used for your reports';
+$_lang['setting_bigbrother.native_app_client_id'] = 'OAuth2.0 Client ID (Desktop/Native App)';
+$_lang['setting_bigbrother.native_app_client_id_desc'] = 'Provide your Client ID from https://console.developers.google.com/apis/credentials';
 
-$_lang['setting_bigbrother.account_name'] = 'Analytics Profile Name';
-$_lang['setting_bigbrother.account_name_desc'] = 'Name of the profile used for your reports';
+$_lang['setting_bigbrother.native_app_client_secret'] = 'OAuth2.0 Client Secret';
+$_lang['setting_bigbrother.native_app_client_secret_desc'] = 'Provide your Client Secret from https://console.developers.google.com/apis/credentials';
 
-$_lang['setting_bigbrother.admin_groups'] = 'Administrator Groups';
-$_lang['setting_bigbrother.admin_groups_desc'] = 'Comma separated list of MODx user groups which have access to the CMP options';
+$_lang['setting_bigbrother.property_id'] = 'GA4 Property ID';
+$_lang['setting_bigbrother.property_id_desc'] = 'This will automatically be filled with your Google Analytics v4 property id when you authorize Big Brother.';
 
-$_lang['setting_bigbrother.cache_timeout'] = 'Cache Timeout';
-$_lang['setting_bigbrother.cache_timeout_desc'] = 'How long result sets from Google Analytics should be cached locally (in seconds)';
+$_lang['setting_bigbrother.refresh_token'] = 'Refresh Token';
+$_lang['setting_bigbrother.refresh_token_desc'] = 'The refresh token is managed internally by Big Brother.';
 
-$_lang['setting_bigbrother.callback_url'] = 'Callback URL';
-$_lang['setting_bigbrother.callback_url_desc'] = 'Callback URL used for authenticating with Google Analytics OAuth services';
+$_lang['setting_bigbrother.assets_url'] = 'Assets URL';
+$_lang['setting_bigbrother.assets_url_desc'] = 'The URL to Big Brother\'s assets directory.';
 
-$_lang['setting_bigbrother.date_begin'] = 'Date Range';
-$_lang['setting_bigbrother.date_begin_desc'] = 'Number of days that will be used for your reports';
+$_lang['setting_bigbrother.assets_path'] = 'Assets Path';
+$_lang['setting_bigbrother.assets_path_desc'] = 'The path to Big Brother\'s assets directory.';
 
-$_lang['setting_bigbrother.date_end'] = 'End date';
-$_lang['setting_bigbrother.date_end_desc'] = 'Tell whether the reports should end today or the day before';
-
-$_lang['setting_bigbrother.oauth_secret'] = 'OAuth Secret Token';
-$_lang['setting_bigbrother.oauth_token'] = 'OAuth Secret';
-
-$_lang['setting_bigbrother.total_account'] = 'Number of Profiles';
-$_lang['setting_bigbrother.total_account_desc'] = 'How many Profiles were found via the GA-API (use internally)';
-
-$_lang['setting_bigbrother.show_visits_on_dashboard'] = 'Dashboard Widget - Visits';
-$_lang['setting_bigbrother.show_visits_on_dashboard_desc'] = 'Show Visits chart on Dashboard';
-
-$_lang['setting_bigbrother.show_metas_on_dashboard'] = 'Dashboard Widget - Metas';
-$_lang['setting_bigbrother.show_metas_on_dashboard_desc'] = 'Show meta informations on Dashboard';
-
-$_lang['setting_bigbrother.show_pies_on_dashboard'] = 'Dashboard Widget - Visitors and Traffic Sources Overview';
-$_lang['setting_bigbrother.show_pies_on_dashboard_desc'] = 'Show Visitors and Traffic Sources overview on Dashboard';
+$_lang['setting_bigbrother.core_path'] = 'Core Path';
+$_lang['setting_bigbrother.core_path_desc'] = 'The path to Big Brother\'s core directory.';
